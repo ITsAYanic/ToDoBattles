c8c26742c7fd94a72dd29b0f01f0338d5ac76011
Yanic <itsayanic@gmail.com>
2025-06-02 11:53:26 +0200

    feat: continued working on the backend

--- crud.py
+++ crud.py
@@ -1,5 +1,26 @@
 from pymongo import MongoClient
-
+from todo import ToDo
 connection_string = "mongodb://localhost:27017/"
 
 client = MongoClient(connection_string)
+#print(client.address)
+
+dbs = client.list_database_names()
+if "lol" in dbs:
+    for db in dbs:
+        print(db)
+
+db = client["todos"]
+collection = db["todos"]
+
+'''
+todo = {"Name": "von Moos"}
+collection.insert_one(todo)
+'''
+collection.delete_many({"Name": "von Moos"})
+datasets = collection.find()
+for data in datasets:
+    print(data)
+
+
+
